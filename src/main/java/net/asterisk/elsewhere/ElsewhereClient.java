6fe1039b183bdd9886ac5d138fe52d9b46820427
Aster <brewstermusician@gmail.com>
2026-01-18 16:37:48 -0600

    :3

---
+++ src/main/java/net/asterisk/elsewhere/ElsewhereClient.java
@@ -0,0 +1,11 @@
+package net.asterisk.elsewhere;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElsewhereClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/asterisk/elsewhere/ElsewhereDataGenerator.java
@@ -0,0 +1,11 @@
+package net.asterisk.elsewhere;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ElsewhereDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
